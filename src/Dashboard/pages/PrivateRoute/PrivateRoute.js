059a87ae0e94ffb9f0ea8c155736207fb0ca0d20
saif maamun <sadifmamun@gmail.com>
2022-04-04 16:27:50 +0600

    private route implemented

--- src/Dashboard/pages/PrivateRoute/PrivateRoute.js
+++ src/Dashboard/pages/PrivateRoute/PrivateRoute.js
@@ -4,13 +4,13 @@ import { Route } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
 
 const PrivateRoute = ({ children, ...rest }) => {
-    const { user, isLoading } = useAuth()
+    const { user} = useAuth()
     
     return (
         <Route
             {...rest}
             render={({ location }) =>
-                user.email ? children : (
+                user.uid ? children : (
                     <Redirect
                         to={{
                             pathname: "/login",
